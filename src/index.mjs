d1e9a87ff416657a60bea692c7ce1683ec86c0db
SunkwiBOT <sunkwi@tuta.io>
2022-11-28 15:23:24 +0100

    update package / add logger

--- src/index.mjs
+++ src/index.mjs
@@ -1,1 +1,17 @@
+/* eslint-disable no-console */
 import {} from 'dotenv/config';
+
+process.on('unhandledRejection', (reason, p) => {
+  console.log(' [Error_Handling] :: Unhandled Rejection/Catch');
+  console.log(reason, p);
+});
+
+process.on('uncaughtException', (err, origin) => {
+  console.log(' [Error_Handling] :: Uncaught Exception/Catch');
+  console.log(err, origin);
+});
+
+process.on('uncaughtExceptionMonitor', (err, origin) => {
+  console.log(' [Error_Handling] :: Uncaught Exception/Catch (MONITOR)');
+  console.log(err, origin);
+});
---
+++ src/logger.mjs
@@ -0,0 +1,41 @@
+import log4js from 'log4js';
+
+log4js.configure({
+  appenders: {
+    console: {
+      type: 'console',
+      level: 'debug',
+      layout: {
+        type: 'pattern',
+        pattern: '%[[%d{dd/MM/yyyy hh:mm:ss}] [%p] | %f{2}:%l:%o | - %m%]'
+      }
+    },
+    everything: {
+      type: 'dateFile',
+      alwaysIncludePattern: true,
+      filename: './log/twitch',
+      pattern: 'yyyy-MM-dd.log',
+      layout: {
+        type: 'pattern',
+        pattern: '[%d{dd/MM/yyyy hh:mm:ss}] [%p] | %f{2}:%l:%o | - %m'
+      }
+    }
+  },
+  categories: {
+    default: {
+      appenders: ['everything', 'console'],
+      level: 'debug',
+      enableCallStack: true
+    }
+  }
+});
+
+const logger = log4js.getLogger();
+
+// logger.info('my info message');
+// logger.warn('my info message');
+// logger.error('my info message');
+// logger.fatal('my info message');
+// logger.mark('my info message');
+
+export default logger 
